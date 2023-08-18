b56058cd53020e2a663d2bcf369e192dd14140cf
Jennavieve Williams <Jennavieve@gendac.co.za>
2023-08-18 15:50:41 +0200

    Exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (sentence, num) {
+    let repeatedSentence = sentence;
+    for (let index = 0; index < num - 1; index++) {
+        repeatedSentence += sentence;
+    }
+    return repeatedSentence;
 };
 
 // Do not edit below this line
